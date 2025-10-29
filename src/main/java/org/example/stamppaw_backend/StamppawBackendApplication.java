f8f8be841ac56f558c6f8d17b5f3789d3b7555f7
gitseoyeon <tjdus553926@gmail.com>
2025-10-29 17:23:56 +0900

    init: 초기세팅

---
+++ src/main/java/org/example/stamppaw_backend/StamppawBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.stamppaw_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StamppawBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StamppawBackendApplication.class, args);
+    }
+
+}
